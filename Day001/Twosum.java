bf65f3e80ad07abb7f7b0c9f06a4b2570795dad7
Adam Desjardins <adamdesjardins@Adams-MacBook-Air.local>
2025-06-29 15:37:20 -0400

    Correction on previously pushed Day001

--- Day001/Twosum.java
+++ Day001/Twosum.java
@@ -1,5 +1,18 @@
 package Twosum;
 
-public class Twosum {
+import java.util.HashMap;
 
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> myHashMap = new HashMap<>();
+        for (int i = 0; i<nums.length; i++) {
+            int complement = target - nums[i];
+            if (myHashMap.containsKey(complement)) {
+                return new int[]{myHashMap.get(complement), i};
+            }
+            myHashMap.put(nums[i], i);
+        }
+    throw new IllegalArgumentException("No two sum solution");
+    }
+    
 }
