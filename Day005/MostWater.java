7aa7f6711e3467ce0f3b18fc1bf53c6bb8399a36
Adam Desjardins <a3.desjardins@gmail.com>
2025-07-07 14:13:20 -0400

    Correction on problem

--- Day005/MostWater.java
+++ Day005/MostWater.java
@@ -1,5 +1,22 @@
-package MostWater;
-
-public class MostWater {
+class Solution {
+    public int maxArea(int[] height) {
+        int right = height.length -1;
+        int left = 0;
+        int maxWater = 0;
+        while (right > left) {
+            int width = right-left;
+            int indexHeight = Math.min(height[left], height[right]);
+            if (width*indexHeight > maxWater) {
+                maxWater = width*indexHeight;
+                }
+            if (height[left] < height[right]) {
+                left++;
+            }
+            else {
+                right--;
+            }
+        }
+    return maxWater;
+     }
 
 }
